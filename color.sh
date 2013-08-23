fb74eedea26af17f19484cc96f6a278e2b1a1abc
chenxingyi <chenxingyi@domob.cn>
2013-08-23 10:18:27 +0800

    1.remove bullshit. 2.use `cat` instead of `read`, as the latter will trim spaces.

--- color.sh
+++ color.sh
@@ -10,12 +10,13 @@ scriptPath=`readlink -f $0`
 scriptDir=`dirname $scriptPath`
 . $scriptDir/terminal-control.sh
 
-echo -e ${CYAN}Reading input...${REG}${REGF}${REGB}
+# okay, this is just noise.
+#echo -e ${CYAN}Reading input...${REG}${REGF}${REGB}
 
-echo -e ${!style}
-while read line; do
-  echo $line
+#while read line; do # nope, `read` will trim spaces
+for line in "`cat`"; do
+  echo -e "${!style}$line${REG}${REGF}${REGB}"
 done
-echo -e ${REG}${REGF}${REGB}
 
-echo -e ${CYAN}Done.${REG}${REGF}${REGB}
+# okay, this is just noise.
+#echo -e ${CYAN}Done.${REG}${REGF}${REGB}
